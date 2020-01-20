47f434ec6b5ea9624aa07acf220a873c20e94144
JuansotoGrupozuniga <juan.soto@grupo-zuniga.com>
2020-01-20 11:52:23 -0500

    Aprobar detalle

---
+++ back-end-temp/Web/logica.minem.gob.pe/NotificacionLN.cs
@@ -0,0 +1,30 @@
+﻿using datos.minem.gob.pe;
+using entidad.minem.gob.pe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica.minem.gob.pe
+{
+    public static class NotificacionLN
+    {
+        public static NotificacionDA notificacionDA = new NotificacionDA();
+
+        public static NotificacionBE ConsultaNotificaciones(int Idusuario, int IdRol)
+        {
+            return notificacionDA.ConsultaNotificaciones(Idusuario, IdRol);
+        }
+
+        public static List<NotificacionBE> ListarNotificacion(NotificacionBE entidad)
+        {
+            return notificacionDA.ListarNotificacion(entidad);
+        }
+
+        public static NotificacionBE RegistraVistoNotificacion(NotificacionBE entidad)
+        {
+            return notificacionDA.RegistraVistoNotificacion(entidad);
+        }
+    }
+}
