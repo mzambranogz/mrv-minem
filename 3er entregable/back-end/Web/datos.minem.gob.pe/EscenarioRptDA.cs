8b308ec693f1e47d1f7121269d29b100f1e119c7
Maycol Zambrano <maycol_630@hotmail.com>
2020-01-24 20:28:39 -0500

    Actualizacion de observaciones subsanadas del Anexo A, enviadas por Carlos

---
+++ 3er entregable/back-end/Web/datos.minem.gob.pe/EscenarioRptDA.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using entidad.minem.gob.pe;
+using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using utilitario.minem.gob.pe;
+
+namespace datos.minem.gob.pe
+{
+    public class EscenarioRptDA : BaseDA
+    {
+        private string sPackage = "USERMRV.PKG_MRV_REPORTES.";
+
+        public List<EscenarioRptBE> ListaEscenariosRpt(EscenarioRptBE entidad)
+        {
+            List<EscenarioRptBE> Lista = null;
+
+            try
+            {
+                using (IDbConnection db = new OracleConnection(CadenaConexion))
+                {
+                    string sp = sPackage + "SP_SEL_ESCENARIOS_RPT";
+                    var p = new OracleDynamicParameters();
+                    p.Add("pIdMedMit", entidad.ID_MEDMIT);
+                    p.Add("pCursor", dbType: OracleDbType.RefCursor, direction: ParameterDirection.Output);
+                    Lista = db.Query<EscenarioRptBE>(sp, p, commandType: CommandType.StoredProcedure).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+
+            return Lista;
+        }
+    }
+}
---
+++ 3er entregable/back-end/Web/logica.minem.gob.pe/EscenarioRptLN.cs
@@ -0,0 +1,20 @@
+﻿using datos.minem.gob.pe;
+using entidad.minem.gob.pe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica.minem.gob.pe
+{
+    public static class EscenarioRptLN
+    {
+        private static EscenarioRptDA escenariorptDA = new EscenarioRptDA();
+
+        public static List<EscenarioRptBE> ListaEscenariosRpt(EscenarioRptBE entidad)
+        {
+            return escenariorptDA.ListaEscenariosRpt(entidad);
+        }
+    }
+}
