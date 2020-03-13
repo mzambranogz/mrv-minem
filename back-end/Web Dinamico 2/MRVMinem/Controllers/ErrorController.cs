93e4a49f3532d168c713f7eb94e2715a4dea5967
JuansotoGrupozuniga <juan.soto@grupo-zuniga.com>
2020-03-13 10:10:53 -0500

    RS - ErrorController

---
+++ back-end/Web Dinamico 2/MRVMinem/Controllers/ErrorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MRVMinem.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            string error = "";
+            string ex = "";
+            //Request.ContentEncoding = System.Text.Encoding.UTF8;
+            //if (Request.Cookies.Get("error") != null) error = Request.Cookies.Get("error").Value;
+            //if (Request.Cookies.Get("ex") != null) ex = Request.Cookies.Get("ex").Value;
+            string viewError = "Default";
+            if (error != null) viewError += error;
+            //TempData["ex"] = ex;
+            return View(viewError);
+        }
+
+        public ActionResult TestError(int valor)
+        {
+            return View();
+        }
+    }
+}
--- back-end/Web Dinamico 2/MRVMinem/Global.asax.cs
+++ back-end/Web Dinamico 2/MRVMinem/Global.asax.cs
@@ -14,5 +14,22 @@ namespace MRVMinem
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+
+            Response.Clear();
+
+            HttpException httpException = exception as HttpException;
+
+            int errorCode = 500;
+
+            if (httpException != null) errorCode = httpException.GetHttpCode();
+
+            Server.ClearError();
+
+            Response.Redirect("~/Error");
+        }
     }
 }
